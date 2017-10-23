cf796bd2825089a611e2dbf3f0cd622ca6c70182
Aurelien Pluche <a.pluche@kreactive.com>
2017-10-23 05:36:17 +0200

    improve handler in begin berserk activity

--- K-cup/app/src/main/java/player/BeginGameBerserkActivity.java
+++ K-cup/app/src/main/java/player/BeginGameBerserkActivity.java
@@ -23,6 +23,19 @@ public class BeginGameBerserkActivity extends AppCompatActivity {
 
     private AnimationDrawable anim;
 
+    private ImageView logo;
+
+    private TextView title;
+    private TextView title2;
+
+    private Animation anim1;
+    private Animation anim2;
+    private Animation anim3;
+
+    private final Handler handler = new Handler();
+    private final Handler handler2 = new Handler();
+    private final Handler handler3 = new Handler();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,64 +46,24 @@ public class BeginGameBerserkActivity extends AppCompatActivity {
 
         LinearLayout container = (LinearLayout) findViewById(R.id.content);
 
-        final ImageView logo = (ImageView) findViewById(R.id.logo);
+        logo = (ImageView) findViewById(R.id.logo);
 
-        TextView title = (TextView) findViewById(R.id.title);
+        title = (TextView) findViewById(R.id.title);
         title.setTypeface(typeface);
 
-        final TextView title2 = (TextView) findViewById(R.id.title_2);
+        title2 = (TextView) findViewById(R.id.title_2);
         title2.setTypeface(typeface);
 
-        final Animation anim1 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.effect_challenge);
+        anim1 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.effect_challenge);
+        anim2 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.effect_disappear);
+        anim3 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.effect_disappear);
+
         title.startAnimation(anim1);
 
         anim = (AnimationDrawable) container.getBackground();
         anim.setEnterFadeDuration(2000);
         anim.setExitFadeDuration(2000);
         anim.setOneShot(true);
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                logo.setVisibility(View.VISIBLE);
-                logo.startAnimation(anim1);
-                title2.setVisibility(View.VISIBLE);
-                title2.startAnimation(anim1);
-            }
-        }, 3000);
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                final Animation anim2 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.effect_disappear);
-
-                anim2.setAnimationListener(new Animation.AnimationListener() {
-                    @Override
-                    public void onAnimationStart(Animation animation) {
-
-                    }
-
-                    @Override
-                    public void onAnimationEnd(Animation animation) {
-                        logo.setVisibility(View.GONE);
-                    }
-
-                    @Override
-                    public void onAnimationRepeat(Animation animation) {
-
-                    }
-                });
-                logo.startAnimation(anim2);
-            }
-        }, 5000);
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                goToNextRule();
-            }
-        }, 7200);
-
     }
 
     private void goToNextRule() {
@@ -106,18 +79,92 @@ public class BeginGameBerserkActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onStop() {
+        super.onStop();
+        handler.removeCallbacksAndMessages(null);
+        handler2.removeCallbacksAndMessages(null);
+        handler3.removeCallbacksAndMessages(null);
+    }
+
+    @Override
     protected void onResume() {
         super.onResume();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (anim1 != null) {
+                    logo.setVisibility(View.VISIBLE);
+                    logo.startAnimation(anim1);
+
+                    anim3.setAnimationListener(new Animation.AnimationListener() {
+                        @Override
+                        public void onAnimationStart(Animation animation) {
+
+                        }
+
+                        @Override
+                        public void onAnimationEnd(Animation animation) {
+                            title.setVisibility(View.INVISIBLE);
+                        }
+
+                        @Override
+                        public void onAnimationRepeat(Animation animation) {
+
+                        }
+                    });
+                    title.startAnimation(anim3);
+
+                    title.setVisibility(View.INVISIBLE);
+                    title2.setVisibility(View.VISIBLE);
+                    title2.startAnimation(anim1);
+                }
+            }
+        }, 3000);
+
+        handler2.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                anim2.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        logo.setVisibility(View.INVISIBLE);
+                        title2.setVisibility(View.INVISIBLE);
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                });
+                logo.startAnimation(anim2);
+                title2.startAnimation(anim2);
+            }
+        }, 5000);
+
+        handler3.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                goToNextRule();
+            }
+        }, 7200);
+
         if (anim != null && !anim.isRunning())
             anim.start();
     }
 
-    // Stopping animation:- stop the animation on onPause.
     @Override
     protected void onPause() {
         super.onPause();
-        if (anim != null && anim.isRunning())
+        if (anim != null && anim.isRunning()) {
             anim.stop();
+        }
     }
 
 }
